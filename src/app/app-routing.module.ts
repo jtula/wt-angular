9c4eee9722b72077ffa1e414e7073aa89aa811c6
Jose Antonio Tula Leyva <joseantonio@jdmlabs.io>
2022-09-26 17:29:56 -0400

    feat: update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProjectComponent } from './add-project/add-project.component';
+import { ContactComponent } from './contact/contact.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditProjectComponent } from './edit-project/edit-project.component';
+import { LoginComponent } from './login/login.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AuthGuard } from './shared/guard/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', title: 'Login', component: LoginComponent },
+  { path: 'projects', title: 'Projects', component: ProjectsComponent, canActivate: [AuthGuard] },
+  { path: 'contact', title: 'Contact Us', component: ContactComponent  },
+  { path: 'dashboard', title: 'Dashboard',component: DashboardComponent, canActivate: [AuthGuard]  },
+  { path: 'add-project', title: 'Add new project', component: AddProjectComponent },
+  { path: 'edit-project/:id', title: 'Edit project',component: EditProjectComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
