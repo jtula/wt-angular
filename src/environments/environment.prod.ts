192167423424332d78a3021641eaead207adbf9e
Jose Antonio Tula Leyva <joseantonio@jdmlabs.io>
2022-09-26 17:29:39 -0400

    conf: add environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
-  production: true
-};
+  firebase: {
+    projectId: 'wt-angular-8d3ea',
+    appId: '1:571229000949:web:461d62ff2af6d66673a1bc',
+    storageBucket: 'wt-angular-8d3ea.appspot.com',
+    locationId: 'us-east1',
+    apiKey: 'AIzaSyDg48UsMv_tbCZ6H9Xf7nhNyXxecY39w_g',
+    authDomain: 'wt-angular-8d3ea.firebaseapp.com',
+    messagingSenderId: '571229000949',
+  },
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDg48UsMv_tbCZ6H9Xf7nhNyXxecY39w_g",
+    authDomain: "wt-angular-8d3ea.firebaseapp.com",
+    projectId: "wt-angular-8d3ea",
+    storageBucket: "wt-angular-8d3ea.appspot.com",
+    messagingSenderId: "571229000949",
+    appId: "1:571229000949:web:461d62ff2af6d66673a1bc"
+  }
+  };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'wt-angular-8d3ea',
+    appId: '1:571229000949:web:461d62ff2af6d66673a1bc',
+    storageBucket: 'wt-angular-8d3ea.appspot.com',
+    locationId: 'us-east1',
+    apiKey: 'AIzaSyDg48UsMv_tbCZ6H9Xf7nhNyXxecY39w_g',
+    authDomain: 'wt-angular-8d3ea.firebaseapp.com',
+    messagingSenderId: '571229000949',
+  },
   production: false
 };
 
